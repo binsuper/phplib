163bb5b4d60f382527fa47634acc326f07b3cc21
Gino Huang <gino@xlcwnet.com>
2022-12-12 15:32:53 +0800

    update

---
+++ src/Validation/Validator.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace Gino\Phplib\Validation;
+
+use Nette\Utils\Validators;
+
+class Validator {
+
+    protected $params   = [];
+    protected $rules    = [];
+    protected $messages = [];
+    protected $v_fails  = [];
+    protected $v_errors = [];
+
+    public function __construct(array $params, array $rules = [], array $messages = []) {
+        $this->params   = $params;
+        $this->rules    = $rules;
+        $this->messages = $messages;
+    }
+
+    /**
+     * @param array $params
+     * @param array $rules
+     * @param array $messages
+     * @return static
+     */
+    public static function make(array $params, array $rules = [], array $messages = []): Validator {
+        return new static($params, $rules, $messages);
+    }
+
+    /**
+     * 按规则对参数进行校验
+     *
+     * @return array
+     */
+    public function validate(): array {
+        $result = [];
+
+        foreach ($this->rules as $field => $rule) {
+            // check field exsits
+            if (!isset($this->params[$field])) {
+                // check required rule
+                if (in_array('required', explode('|', $rule))) {
+                    // errors
+                    $this->v_fails[$field]  = $rule;
+                    $this->v_errors[$field] = $this->getFailMessage($field, $rule, true);
+                }
+                continue;
+            }
+
+            $field_value = $this->params[$field];
+
+            // validate params
+            if (!Validators::is($field_value, $rule)) {
+                $this->v_fails[$field]  = $rule;
+                $this->v_errors[$field] = $this->getFailMessage($field, $rule, false, $field_value);
+                continue;
+            }
+
+            // finish
+            $result[$field] = $field_value;
+        }
+
+        return $result;
+    }
+
+    /**
+     * @param string $field
+     * @param $expected
+     * @param $type
+     * @return string
+     */
+    protected function getFailMessage(string $field, string $expected, $miss = false, $val = null): string {
+        if ($miss) {
+            $msg = "Missing variable $field";
+        } else {
+            $type = gettype($val);
+            $msg  = "The $field expects to be $expected, $type given.";
+        }
+        return $this->messages[$field] ?? $msg;
+    }
+
+    /**
+     * Determine if the data fails the validation rules.
+     *
+     * @return bool
+     */
+    public function fails(): bool {
+        return !empty($this->v_fails);
+    }
+
+    /**
+     * 获取失败的规则
+     *
+     * @return array
+     */
+    public function failed(): array {
+        return $this->v_fails;
+    }
+
+    /**
+     * 获取所有错误消息
+     *
+     * @return array
+     */
+    public function errors(): array {
+        return $this->v_errors;
+    }
+
+}
